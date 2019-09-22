f1aceb4b2e1fe7a2e44549ede5c2c4a7d406f8ef
wenshuai <18336136677@163.com>
2019-09-22 15:41:53 +0800

    实现了对User表的增删改查，mybatis初步学习成功！

--- src/com/qin/test/UserTest.java
+++ src/com/qin/test/UserTest.java
@@ -1,6 +1,8 @@
 package com.qin.test;
 
 import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -26,6 +28,79 @@ public class UserTest {
 		System.out.println(user);
 		openSession.close();
 	} 
+	
+	@Test
+	public void testFindUserbyUserName() throws Exception{
+		String resource = "SqlMapConfig.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+		SqlSession openSession = factory.openSession();
+		
+		List<User> list = openSession.selectList("test.findUserByUserName", "王");
+		System.out.println(list);
+	}
+	
+    @Test
+	public void testInsertUser() throws Exception{
+		String resource = "SqlMapConfig.xml";
+		//通过流将核心配置文件读取进来
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		//通过核心配置文件输入流来创建会话工厂
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+		//通过工厂创建会话
+		SqlSession openSession = factory.openSession();
+		
+		User user = new User();
+		user.setUsername("赵四");
+		user.setBirthday(new Date());
+		user.setSex("1");
+		user.setAddress("北京昌平");
+		System.out.println("====" + user.getId());
+		
+		openSession.insert("test.insertUser", user);
+		//提交事务(mybatis会自动开启事务,但是它不知道何时提交,所以需要手动提交事务)
+		openSession.commit();
+		
+		System.out.println("====" + user.getId());
+	} 
+    
+    @Test
+	public void testDelUserById()throws Exception{
+		String resource = "SqlMapConfig.xml";
+		//通过流将核心配置文件读取进来
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		//通过核心配置文件输入流来创建会话工厂
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+		//通过工厂创建会话
+		SqlSession openSession = factory.openSession();
+		
+		openSession.delete("test.delUserById", 27);
+		//提交
+		openSession.commit();
+	}
+    
+    @Test
+	public void testUpdateUserById() throws Exception{
+		String resource = "SqlMapConfig.xml";
+		//通过流将核心配置文件读取进来
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		//通过核心配置文件输入流来创建会话工厂
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+		//通过工厂创建会话
+		SqlSession openSession = factory.openSession();
+		
+		User user = new User();
+		user.setId(26);
+		user.setUsername("王麻子");
+		openSession.update("test.updateUserById", user);
+		
+		//提交
+		openSession.commit();
+	}
+
+
+
+
 
 
 }
